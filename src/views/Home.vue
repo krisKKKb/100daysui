449ac28cde24aa0339fafe85ea26ccf42069550f
kriskkkb <krisburk577@gmail.com>
2021-01-14 15:29:28 +0200

    changes to sign up

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,32 @@
 <template>
-  <div class="flex-center">
-    <div class="home-content">
-      <div class="signupcontent">
-        <h1>Sign up form</h1>
-        <div class="usern">
-          Kasutajanimi
-          <input name="username" placeholder="Sisestage kasutajanimi" />
-        </div>
-        <div class="pw">
-          Parool
-          <input
-            name="password"
-            placeholder="Sisestage parool"
-            type="password"
-          />
-        </div>
-        <div class="signup">
-          <button class="signupbtn">Sign up</button>
+  <body>
+    <div class="flex-center">
+      <div class="home-content">
+        <div class="signupcontent">
+          <h1>Sign up form</h1>
+          <div class="email">
+            E-mail
+            <input name="email" placeholder="Sisestage e-mail" />
+          </div>
+          <div class="usern">
+            Kasutajanimi
+            <input name="username" placeholder="Sisestage kasutajanimi" />
+          </div>
+          <div class="pw">
+            Parool
+            <input
+              name="password"
+              placeholder="Sisestage parool"
+              type="password"
+            />
+          </div>
+          <div class="signup">
+            <button class="signupbtn">Sign up</button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </body>
 </template>
 
 <script>
@@ -33,6 +39,10 @@ export default {
 </script>
 
 <style lang="scss">
+body {
+  background: linear-gradient(0.25turn, #00f208, #00fad0, #00aeff);
+}
+
 .flex-center {
   display: flex;
   justify-content: center;
@@ -43,8 +53,13 @@ export default {
     min-width: 400px;
   }
 }
-.signupcontent {
-  background: lightgray;
+.email {
+  display: flex;
+  margin-left: 7.5rem;
+  flex-direction: column;
+  justify-content: center;
+  padding-top: 1rem;
+  width: 10rem;
 }
 .pw {
   display: flex;
@@ -66,9 +81,11 @@ export default {
   padding: 1.5rem;
 }
 .signupbtn {
-  background-color: lightgreen;
+  background: linear-gradient(#08c94c, #21ff6f, #bfffd6);
   width: 250px;
   height: 50px;
   font-size: 1rem;
+  border-radius: 100px;
+  text-decoration: none;
 }
 </style>
