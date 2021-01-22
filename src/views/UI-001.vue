6cfcd3dc8d32de7957c68983dc305291d0a45eba
kriskkkb <krisburk577@gmail.com>
2021-01-22 12:58:33 +0200

    ol

--- src/views/UI-001.vue
+++ src/views/UI-001.vue
@@ -1,12 +1,74 @@
 <template>
-  <div class="flex-center">
-    <div class="home-content">
-      <h1>Siia tuleb esimene ülesanne (sign-up form)</h1>
-      <p>
-        Challenge: Design Hint... Create a sign up page, modal, form, or app
-        screen related to signing up for something. It could be for a volunteer
-        event, contest registration, a giveaway, or anything you can image.
-      </p>
+  <div>
+    <h2>Countdown timer</h2>
+    <h1>{{ parseSeconds(countDown) }}</h1>
+    <div>
+      <button type="button" @click="rem(3600)">- hr</button>
+      <button type="button" @click="add(3600)">+ hr</button>
+      <button type="button" @click="rem(60)">- min</button>
+      <button type="button" @click="add(60)">+ min</button>
+      <button type="button" @click="rem(1)">- s</button>
+      <button type="button" @click="add(1)">+ s</button>
+    </div>
+    <div>
+      <button type="button" @click="setRunning(true)">Start</button>
+      <button type="button" @click="setRunning(false)">Stop</button>
+      <button type="button" @click="reset()">Reset</button>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      countDown: 60,
+      running: false,
+    };
+  },
+  methods: {
+    setRunning(bool) {
+      this.running = bool;
+    },
+    parseSeconds(seconds) {
+      return new Date(seconds * 1000).toISOString().substr(11, 8);
+    },
+    countDownTimer() {
+      if (this.countDown > 0 && this.running) {
+        setTimeout(() => {
+          this.countDown > 0 ? (this.countDown -= 1) : 0;
+          this.countDownTimer();
+        }, 1000);
+      }
+      if (this.countDown <= 0) {
+        this.running = false;
+      }
+    },
+    add(sec) {
+      this.countDown += parseInt(sec);
+    },
+    rem(sec) {
+      if (this.countDown >= parseInt(sec)) {
+        this.countDown -= parseInt(sec);
+      }
+    },
+    reset() {
+      this.running = false;
+      this.countDown = 0;
+    },
+  },
+  watch: {
+    running: function () {
+      this.countDownTimer();
+    },
+  },
+  created() {},
+};
+</script>
+
+<style scoped>
+.img {
+  width: 300px;
+  height: auto;
+}
+</style>
